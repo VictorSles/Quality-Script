e367d96ec023faf206a92d94c495e55b96576631
Victor_Sales <israelvictords@gmail.com>
2025-11-17 19:00:12 -0400

    up

--- SCRIPTS DE TESTE/teste 2.py
+++ SCRIPTS DE TESTE/teste 2.py
@@ -1,5 +1,8 @@
-import os
-import subprocess
-import tkinter
-
-root = 
+# pingar na URL do servidor (subprocess)
+# solicitar login e senha (GUI)
+# retornar o token
+# armazenar em um txt definido pelo usuário
+# 
+# * URL´s globais
+# * Paradigma funcional
+# 
