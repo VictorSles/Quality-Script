50a78e2d9d31a29a7ce1c172d7962d09c80ed28b
Victor_Sales <israelvictords@gmail.com>
2025-11-17 21:08:35 -0400

    up

--- SCRIPTS DE TESTE/teste 2.py
+++
@@ -1,8 +0,0 @@
-# pingar na URL do servidor (subprocess)
-# solicitar login e senha (GUI)
-# retornar o token
-# armazenar em um txt definido pelo usuário
-# 
-# * URL´s globais
-# * Paradigma funcional
-# 
